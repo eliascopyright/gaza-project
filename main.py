0dd9296154ddd79bc6c2637263cd6ceb99de3c99
eliascopyright <eliascopyrightl@gmail.com>
2026-05-09 11:27:23 +0200

    fix: "deck file returned well"

--- main.py
+++ main.py
@@ -2,41 +2,63 @@ from pathlib import	Path
 import os, sys
 
 import	yaml, geopandas	as gpd, requests, zipfile, argparse
+from app.app import serve_map
+import uvicorn
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 from gaza_etl.extract import	extract_all
 from gaza_etl.transform	import	convert_all
 from gaza_etl.download	import	extract_from_db
 from gaza_etl.map	import	make_map
+from gaza_etl.map_kepler import	make_map_kepler
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import RedirectResponse
 
 
 
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="maps"), name="static")
+
+@app.get("/map", response_class=HTMLResponse)
+def serve_map():
+    cfg = load_cfg()
+    deck = make_map_kepler(cfg)
+    deck_html = deck.to_html()
+    deck.to_html("maps/gaza_layers.html")
+    return RedirectResponse(url = "/static/gaza_layers.html")
+  
+   
 BASE_DIR = Path(__file__).resolve().parent / "gaza_etl"
 CONFIG_PATH = BASE_DIR / "config.yaml"
 
 def load_cfg():
-	with open(CONFIG_PATH, "r") as f:
-		return yaml.safe_load(f)
-	
+ with open(CONFIG_PATH, "r") as f:
+  return yaml.safe_load(f)
+ 
 def main():
-	p = argparse.ArgumentParser(description="Gaza Evidence ETL")
-	p.add_argument("cmd", choices=["run-all", "download", "extract", "transform", "map"])
-	p.add_argument("--config", default=CONFIG_PATH, help="Chemin du fichier de config")
-	args = p.parse_args()
+ p = argparse.ArgumentParser(description="Gaza Evidence ETL")
+ p.add_argument("cmd", choices=["run-all", "download", "extract", "transform", "map", "serve"])
+ p.add_argument("--config", default=CONFIG_PATH, help="Chemin du fichier de config")
+ args = p.parse_args()
 
-	cfg = load_cfg()
-	
-	# (facultatif) affiche où on écrit les fichiers
-	print("Working dir:", Path.cwd().resolve())
-	print("Config file:", Path(args.config).resolve())
-	print("Paths:", cfg.get("paths"))
+ cfg = load_cfg()
+ 
+ # (facultatif) affiche où on écrit les fichiers
+ print("Working dir:", Path.cwd().resolve())
+ print("Config file:", Path(args.config).resolve())
+ print("Paths:", cfg.get("paths"))
 
-	if args.cmd in ("download", "run-all"):
-					extract_from_db()
-	if args.cmd in ("extract", "run-all"):
-					extract_all(cfg)
-	if args.cmd in ("transform", "run-all"):
-					convert_all(cfg)
-	if args.cmd in ("map", "run-all"):
-					make_map(cfg)
+ if args.cmd in ("download", "run-all"):
+     extract_from_db()
+ if args.cmd in ("extract", "run-all"):
+     extract_all(cfg)
+ if args.cmd in ("transform", "run-all"):
+     convert_all(cfg)
+ if args.cmd in ("map", "run-all"):
+     make_map_kepler(cfg)
+ if args.cmd == "serve":
+    uvicorn.run(app, host= "0.0.0.0", port=8000)
+    
 
 if __name__ == "__main__":
-	main()
+ main()
